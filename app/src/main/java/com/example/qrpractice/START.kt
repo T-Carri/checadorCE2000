c4bb5ca1ac706b2239bc628dbc0a78fc84a9ea9d
T-Carri <bryan_narutito@hormail.com>
2022-07-07 16:21:04 -0500

    RED ADDED

--- app/src/main/java/com/example/qrpractice/START.kt
+++ app/src/main/java/com/example/qrpractice/START.kt
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.qrpractice.R
 import java.text.DateFormat
 import java.util.*
@@ -13,6 +19,8 @@ class START : AppCompatActivity() {
 
     private var spLenguajes: Spinner?=null
     var vt: TextView? = null
+    var t: TextView? = null
+    private var requestQueue: RequestQueue?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,10 +45,24 @@ btn.setOnClickListener{
         vt = findViewById<View>(R.id.fecha) as TextView
         val mydate = DateFormat.getDateTimeInstance().format(Calendar.getInstance().time)
         vt!!.text = mydate
+
+
         //handle operation data for spinner and project
+        val textView = findViewById<TextView>(R.id.textView)
+        // ...
 
-        
-    }
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://www.google.com"
 
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response.substring(0, 1)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
 
-}
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+}}
