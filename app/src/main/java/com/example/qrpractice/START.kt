a9685d870200aded6af17dea6c84420e506a02e1
T-Carri <bryan_narutito@hormail.com>
2022-07-08 18:10:49 -0500

    JSONArray

--- app/src/main/java/com/example/qrpractice/START.kt
+++ app/src/main/java/com/example/qrpractice/START.kt
@@ -9,6 +9,7 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.qrpractice.R
@@ -53,24 +54,23 @@ btn.setOnClickListener{
         val textView = findViewById<TextView>(R.id.textView)
         // ...
 
-        // Instantiate the RequestQueue.
+    // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val url = "https://www.google.com"
+        val url = "https://my-json-server.typicode.com/typicode/demo/profile"
 
         // Request a string response from the provided URL.
-        val stringRequest = StringRequest(Request.Method.GET, url,
+      /*  val stringRequest = StringRequest(Request.Method.GET, url,
             Response.Listener<JSONArray> {
-                    val gson = Gson()
-
-                    response->
-                textView.text=
-
-                  /*  response ->
-                // Display the first 500 characters of the response string.
-                textView.text = "Response is: ${response.substring(0, 1)}"*/
+                /*  response ->
+// Display the first 500 characters of the response string.
+textView.text = "Response is: ${response.substring(0, 1)}"*/
+            }*/
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                textView.text = "Response: %s".format(response.toString())
             },
             Response.ErrorListener { textView.text = "That didn't work!" })
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest)
+        queue.add(jsonObjectRequest)
 }}
